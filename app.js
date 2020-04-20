97508164b58d8576ddde61796b78a456dd05602b
Daniel Evangelista Pereira <daniel.evangelista93@gmail.com>
2020-04-20 20:37:02 -0300

    finalizado logica de registro de logs - aula 65 e 66

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ new Vue({
         return {
             running: false,
             playerLife: 100,
-            monsterLife: 100
+            monsterLife: 100,
+            logs: []
         }
     },
     watch: {
@@ -20,29 +21,41 @@ new Vue({
     methods: {
         startGame() {
             this.running = true;
+            this.logs = [];
             this.playerLife = 100;
             this.monsterLife = 100;
         },
         attack(especial) {
-            this.hurt('playerLife', 7, 12, false);
-            this.hurt('monsterLife', 5, 10, especial);
+            this.hurt('monsterLife', 5, 10, especial, 'Monstro', 'Jogador', 'monster');
+
+            if (this.monsterLife > 0) {
+                this.hurt('playerLife', 7, 12, false, 'Jogador', 'Monstro', 'player');
+            }
         },
-        hurt(prop, min, max, especial) {
+        hurt(prop, min, max, especial, source, target, cls) {
             const plus = especial ? 5 : 0;
             const hurt = this.getRandom(min + plus, max + plus);
             this[prop] = Math.max(this[prop] - hurt, 0);
+            this.registerLog(`${source} atingiu ${target} com ${hurt}.`, cls);
         },
         healAndHurt() {
             this.heal(10, 15);
-            this.hurt('playerLife', 7, 12, false);
+            this.hurt('playerLife', 7, 12, false, 'Monstro', 'Jogador', 'monster');
         },
         heal(min, max) {
             const heal = this.getRandom(min, max);
+            this.registerLog(`Jogador ganhou força de ${heal}.`, 'player');
             this.playerLife = Math.min(this.playerLife + heal, 100);
         },
         getRandom(min, max) {
             const value = Math.random() * (max - min) + min;
             return Math.round(value);
+        },
+        registerLog(text, cls) {
+            this.logs.unshift({
+                text,
+                cls
+            });
         }
     },
 });
